05f026d1ee72ea787d051ca752f8577ad3f29db0
Felipe Figueiredo <philsf79@gmail.com>
2020-10-13 01:46:38 -0300

    colunas camptodactilia

--- scripts/input.R
+++ scripts/input.R
@@ -22,3 +22,13 @@ setnames(dados, "CorrecaoInd", "CorrecaoInD")
 setnames(dados, "CorrecaoAndi", "CorrecaoAnD")
 setnames(dados, "AnDi", "AnD")
 setnames(dados, "TempoAcompanhamentoAnDi", "TempoAcompanhamentoAnD")
+
+# camptodacilia
+setnames(dados, "InD", "CamptodactiliaInD")
+setnames(dados, "InE", "CamptodactiliaInE")
+setnames(dados, "MeD", "CamptodactiliaMeD")
+setnames(dados, "MeE", "CamptodactiliaMeE")
+setnames(dados, "AnD", "CamptodactiliaAnD")
+setnames(dados, "AnE", "CamptodactiliaAnE")
+setnames(dados, "MiD", "CamptodactiliaMiD")
+setnames(dados, "MiE", "CamptodactiliaMiE")
